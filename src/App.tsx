dc7e8c6ac478e274395d7c1361470960a7c6a93b
b-sandrine <sanrdushimimana@gmail.com>
2023-05-03 21:43:55 +0200

    Added full loading component

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,21 @@ import Card from './component/Card/Card'
 import AddItem from './component/AddItem/AddItem'
 import MainLoading from './component/Loading/MainLoading'
 import { Route, Routes  } from 'react-router'
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
 
 function App() {
+
+  const [isLoading, setIsLoading ] = useState(true);
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(!isLoading)
+      navigate('/card')
+    },5000)
+    return () => clearTimeout(timer)
+  },[isLoading, navigate])
 
   return (
     <div className='main--container'>
--- src/component/Loading/MainLoading.tsx
+++ src/component/Loading/MainLoading.tsx
@@ -1,6 +1,8 @@
 import './MainLoading.css'
 import Loading_image from '../../assets/loading-image.png'
+
 export default function MainLoading () {
+    
     return (
         <div className="mainloading--container">
             <img src={Loading_image} alt="A place holder image for loading page" />
