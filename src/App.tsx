8f81c566286027faf8f4529dbd896bae4daedb24
b-sandrine <sanrdushimimana@gmail.com>
2023-05-04 10:50:39 +0200

    Login page details

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,22 @@ import Card from './component/Card/Card'
 import AddItem from './component/AddItem/AddItem'
 import MainLoading from './component/Loading/MainLoading'
 import { Route, Routes  } from 'react-router'
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router'
+// import { useState, useEffect } from 'react'
+// import { useNavigate } from 'react-router'
 import Login from './component/Login/Login'
 
 function App() {
 
-  const [isLoading, setIsLoading ] = useState(true);
-  const navigate = useNavigate()
+  // const [isLoading, setIsLoading ] = useState(true);
+  // const navigate = useNavigate()
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(!isLoading)
-      navigate('/card')
-    },5000)
-    return () => clearTimeout(timer)
-  },[isLoading, navigate])
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(!isLoading)
+  //     navigate('/card')
+  //   },5000)
+  //   return () => clearTimeout(timer)
+  // },[isLoading, navigate])
 
   return (
     <div className='main--container'>
--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -3,7 +3,16 @@ import './Login.css'
 export default function Login () {
     return (
         <div className="login--container">
-            Log In page
+            <h1>Welcome back!</h1>
+            <form action="">
+                <label>username
+                    <input type="text" name="username" id="" />
+                </label>
+                <label>password:
+                    <input type="password" name="" id="" />
+                </label>
+                <input type="submit" name="" id="" value='Log In' />
+            </form>
         </div>
     )
 }
